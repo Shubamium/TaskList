543bba6aba802e8429490604e564ee7ce959eb48
Vincent Liuss <liusvinv@gmail.com>
2022-12-17 01:48:24 +0700

    Integrate Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   
+   <h2 className="text-3xl underline font-bold font-sans text-blue-400 bg-cyan-100 px-3 max-w-md mx-auto py-7 rounded-md">Hello World!</h2>
   )
 }
 
