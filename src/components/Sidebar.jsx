abb8406fee4c423b227f8ec5973f80f790c4c536
Vincent Liuss <liusvinv@gmail.com>
2022-12-19 05:32:12 +0700

    Add the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLayerGroup} from "@fortawesome/free-solid-svg-icons";
+import { faLayerGroup,faNoteSticky} from "@fortawesome/free-solid-svg-icons";
 const Sidebar = () => {
     return ( 
-        <div className="sidebar shadow-lg bg-slate-700">
+        <div className="sidebar shadow-lg bg-slate-700 h-screen flex flex-col justify-items-stretch">
             <div className="section w-full py-2 px-1 bg-blue-600">
-                <button className=""><FontAwesomeIcon icon={faLayerGroup} /></button>
-                <h2 className="font-poppins text-blue-400 text-lg font-bold">Task Group</h2>
-                <FontAwesomeIcon icon="fa-solid fa-layer-group" />
-                <FontAwesomeIcon icon="fa-solid fa-coffee" />
-                <FontAwesomeIcon icon="fa-solid fa-layer-group" />
+                <div className="flex items-center">
+                    <button className=""><FontAwesomeIcon icon={faLayerGroup} className='text-sky-200 text-2xl bg-blue-900 p-4 rounded-lg hover:rounded-2xl transition-all' /></button>
+                    <h2 className="mx-4 whitespace-nowrap font-poppins text-blue-200 text-2xl font-bold hover-show">Task Group</h2>
+                </div>
             </div>
-            <button>222</button>
-            <button>3</button>
+            <div className="group-list p-1 py-4 grid gap-4 overflow-auto overflow-x-hidden">
+                <div className="flex items-center hover:bg-blue-800 cursor-pointer mx-1">
+                    <button className="text-sky-200 text-3xl bg-blue-600 p-4 rounded-lg hover:rounded-2xl transition-all "><FontAwesomeIcon icon={faNoteSticky}/></button>
+                    <h2 className="mx-4  font-poppins text-blue-200 text-xl font-bold hover-show">Group Name 1</h2>
+                </div>
+                <div className="flex items-center hover:bg-blue-800 cursor-pointer mx-1">
+                    <button className="text-sky-200 text-3xl bg-blue-600 p-4 rounded-lg hover:rounded-2xl transition-all "><FontAwesomeIcon icon={faNoteSticky}/></button>
+                    <h2 className="mx-4  font-poppins text-blue-200 text-xl font-bold hover-show">Group Name 1</h2>
+                </div> <div className="flex items-center hover:bg-blue-800 cursor-pointer mx-1">
+                    <button className="text-sky-200 text-3xl bg-blue-600 p-4 rounded-lg hover:rounded-2xl transition-all "><FontAwesomeIcon icon={faNoteSticky}/></button>
+                    <h2 className="mx-4  font-poppins text-blue-200 text-xl font-bold hover-show">Group Name 1</h2>
+                </div> <div className="flex items-center hover:bg-blue-800 cursor-pointer mx-1">
+                    <button className="text-sky-200 text-3xl bg-blue-600 p-4 rounded-lg hover:rounded-2xl transition-all "><FontAwesomeIcon icon={faNoteSticky}/></button>
+                    <h2 className="mx-4  font-poppins text-blue-200 text-xl font-bold hover-show">Group Name 1</h2>
+                </div>
+               
+            </div>
+
         </div>
      );
 }
