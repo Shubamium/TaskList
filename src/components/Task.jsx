c7c013023d9f0449714966047eeeff45df592d04
Vincent Liuss <liusvinv@gmail.com>
2022-12-18 19:33:06 +0700

    Fix the styling on the task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -18,8 +18,8 @@ const Task = ({task,completed, remove}) => {
         });
     };
     return ( 
-        <div className={`${!task.finished ?  'bg-sky-500' : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto px-9 shadow-sm rounded-md hover:shadow-lg border-2 border-slate-700 inset-5 hover:translate-x-3 transition-all hover:outline`} >
-            <h2 className="font-poppins text-2xl font-bold text-sky-900 my-0">{task.taskName}</h2>
+        <div className={`${!task.finished ?  'bg-sky-500' : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto px-9 max-w-full overflow-hidden shadow-sm rounded-md hover:shadow-lg border-2 border-slate-700 inset-5 hover:translate-x-3 transition-all hover:outline`} >
+            <h2 className="font-poppins text-2xl font-bold text-sky-900 my-0 break-words">{task.taskName}</h2>
             <p className="font-poppins text-slate-800 text-xs font-light">{task.hasOwnProperty('created') && task.created}</p>
             <p className="font-poppins font-medium">{task.taskDes}</p>
             
