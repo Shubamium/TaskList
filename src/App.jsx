6487c31a7a706fbd4ad1deba5b4dec0906239fdb
Vincent Liuss <liusvinv@gmail.com>
2022-12-18 18:09:19 +0700

    Add the timestamp functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
       id:0,
       taskName:"This is a sample Task!",
       taskDes:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-      finished:false
+      finished:false,
+      created:'aaaa'
     }
   ];
 
@@ -59,10 +60,12 @@ function App() {
   const addTask = (name,des) =>{
     setTaskList((tl)=>{
       let updated = [...tl];
+      let newDate = new Date();
       updated.push({
         id: updated.length,
         taskName:name,
-        taskDes:des
+        taskDes:des,
+        created: newDate.toUTCString()
       })
       return updated;
     });
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -18,12 +18,13 @@ const Task = ({task,completed, remove}) => {
         });
     };
     return ( 
-        <div className={`${!task.finished ?  'bg-sky-500' : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto shadow-sm rounded-md hover:shadow-lg border-2 border-slate-700 inset-5 hover:translate-x-3 transition-all hover:outline`} >
-            <h2 className="font-poppins text-xl font-bold text-sky-900 my-0">{task.taskName}</h2>
-            <p className="font-poppins">{task.taskDes}</p>
+        <div className={`${!task.finished ?  'bg-sky-500' : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto px-9 shadow-sm rounded-md hover:shadow-lg border-2 border-slate-700 inset-5 hover:translate-x-3 transition-all hover:outline`} >
+            <h2 className="font-poppins text-2xl font-bold text-sky-900 my-0">{task.taskName}</h2>
+            <p className="font-poppins text-slate-800 text-xs font-light">{task.hasOwnProperty('created') && task.created}</p>
+            <p className="font-poppins font-medium">{task.taskDes}</p>
             
-            <button className="bg-sky-800 hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={completed}>Completed</button>
-            <button className="bg-rose-500 hover:bg-rose-700 hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={animateExit}>Remove</button>
+            <button className="bg-sky-800 shadow-md text-xs hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={completed}>Completed</button>
+            <button className="bg-rose-500 shadow-md text-xs hover:bg-rose-600 hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={animateExit}>Remove</button>
         </div> 
      );
 }
