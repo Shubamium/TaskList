ed8f7bc692b716bdd7e0b525bb252caac2e6b0ea
Vincent Liuss <liusvinv@gmail.com>
2022-12-21 04:24:44 +0700

    Add more styling to the completed task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -19,9 +19,9 @@ const Task = ({task,completed, remove, update}) => {
         });
     };
 
-    let color = task.color === undefined ? `color-${ColorList()[10]}` : 'bg-sky-500';
+    let color = task.color !== undefined ? `color-${ColorList()[task.color]}` : 'bg-sky-500';
     return ( 
-        <div className={`${!task.finished ?  color : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto px-9 max-w-full overflow-hidden shadow-sm rounded-md hover:shadow-lg border-2 border-slate-700 inset-5 hover:translate-x-3 transition-all hover:outline`} >
+        <div className={`${!task.finished ?  color : 'bg-emerald-500'} task p-2 text-sky-100 text-center my-4 mx-auto px-9 max-w-full overflow-hidden shadow-sm rounded-md hover:shadow-lg border-2 ${task.finished ? ' border-slate-200 border-dashed' : 'border-slate-700'} inset-5 hover:translate-x-3 transition-all hover:outline`} >
             <h2 className="font-poppins text-2xl font-bold text-sky-900 my-0 break-words">{task.taskName}</h2>
             <p className="font-poppins text-slate-800 text-xs font-light">
              {task.hasOwnProperty('created') && task.created} 
@@ -30,7 +30,7 @@ const Task = ({task,completed, remove, update}) => {
              </p>
             <p className="font-poppins font-medium break-words">{task.taskDes}</p>
             
-            <button className="bg-sky-800 shadow-md text-xs hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={completed}>Completed</button>
+            <button className="bg-sky-800 shadow-md text-xs hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={completed}>{task.finished? 'Cancel' :'Completed'}</button>
             <button className="bg-slate-600 shadow-md text-xs hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={update}>Edit</button>
             <button className="bg-rose-500 shadow-md text-xs hover:bg-rose-600 hover:scale-110 active:scale-95 p-2 m-2 font-bold font-poppins rounded-sm transition-all" onClick={animateExit}>Remove</button>
         </div> 
