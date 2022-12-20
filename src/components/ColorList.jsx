c979cf8d4a79555576285f765d9399fbdc0dea8e
Vincent Liuss <liusvinv@gmail.com>
2022-12-21 03:50:20 +0700

    Add color list

---
+++ src/components/ColorList.jsx
@@ -0,0 +1,6 @@
+const ColorList = () => {
+    let colorList = ['red-500','orange-500','yellow-300','lime-400','emerald-500'];
+    return colorList;
+}
+  
+export default ColorList;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,14 @@
-
+import ColorList from "./ColorList";
 const Modal = ({isOpen,onHide,children}) => {
     const closeButton = ()=>{
         return  <button onClick={onHide} className="block bg-rose-400 font-poppins rounded-sm font-bold p-2 m-4 shadow-md relative text-red-900"> X </button>;
     }
+
     return (
       <>
         {isOpen && (
           <div className="bg-slate-800 fixed w-full h-full z-50 flex justify-center items-center  modal panel-reoffset">
-              <div className="panel bg-blue-600 ">
+              <div className="panel bg-blue-600" >
                     <div className="action w-100 flex justify-end">
                         {closeButton()}
                     </div>
